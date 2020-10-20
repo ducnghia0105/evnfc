b043aeae2012b815d43cae068200b1e2c8fdf601
root <root@vps22575.sdns.vn>
2020-10-20 16:23:42 +0700

    UPLOAD + THEME + NEW DB 20-10-4PM

---
+++ themes/evn-phongmy/page.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<?php the_content(); ?>
+
+
+	<!-- 
+	<section class="page-title section-bg" style="background-image: url(http://evnfc.vn/themes/evf2020/assets/images/page/chinh-sach-chat-luong.jpg);">
+			<div class="page-title__inner">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 col-xl-6">
+							<div class="page-title__content">
+								<span class="page-title__subtitle"> Chính sách chất lượng </span>
+								<h2 class="page-title__title">Cung cấp đầy đủ dịch vụ và các tiện ích tốt nhất cho khách hàng</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+
+
+
+
+	<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="page-content" style="padding: 30px 0px;">
+						
+					</div>
+				</div>
+
+			</div>
+	</div> -->
+
+
+<?php get_footer(); ?>
